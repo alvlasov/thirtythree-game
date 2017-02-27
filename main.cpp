87ac658fcbfcd59408b8c34a384f9ae388b17362
Alexander <outface.rus@gmail.com>
2017-02-27 12:53:47 +0300

    Some unit-tests added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,61 @@
 #include <iostream>
 #include <Stack.cpp>
 
-int main()
+#define TEST(what,cond)\
+    if (cond())                 \
+        cout << "Test passed!" << endl; \
+    else                        \
+        cout << "Test not passed!" << endl; \
+
+#define ASSERT_EQ(cond1,cond2)  \
+    if (cond1!=cond2)           \
+        assert(0);              \
+
+#define EXCEPT_EQ(cond1,cond2)  \
+    if (cond1!=cond2)           \
+        return false;           \
+
+bool PushSingleNumber()
+{
+    Stack s;
+    s.push(3802);
+    EXCEPT_EQ(s.top(), 0xEDA);
+    s.pop();
+    EXCEPT_EQ(s.size(), 0);
+    return true;
+}
+
+bool DestroyedStack()
 {
     Stack *p;
+    {
+        Stack s;
+        for (int i = 0; i < s.capacity(); i++)
+            s.push(100 + i);
+        p = &s;
+    }
+    EXCEPT_EQ(p->ok(), false);
+    return true;
+}
+
+bool CannotPushMore()
+{
+    Stack s;
+    for (int i = 0; i < s.capacity(); i++)
+        s.push(100 + i);
+    EXCEPT_EQ(s.push(0xF00D), false);
+    return true;
+}
+
+
+int main()
+{
+    Stack s;
+    TEST(Stack, PushSingleNumber);
+    TEST(Stack, CannotPushMore);
+    TEST(Stack, DestroyedStack);
+
+    /*Stack *p;
     {
         Stack s(8);
         s.push(10);
@@ -27,6 +79,6 @@ int main()
     cout << s1.empty() << endl;
     cout << s1.pop() << endl;
     cout << s1.empty() << endl;
-    s1.dump();
+    s1.dump();*/
     return 0;
 }
