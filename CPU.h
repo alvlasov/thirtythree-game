49cba54576d43289f861623a027913b0b90027ce
Alexander <outface.rus@gmail.com>
2017-04-11 23:11:17 +0300

    Каркас класса CPU

---
+++ CPU.h
@@ -0,0 +1,33 @@
+#ifndef CPU_H_INCLUDED
+#define CPU_H_INCLUDED
+
+namespace thirtythree
+{
+    class CPU
+    {
+    public:
+
+        CPU();
+        ~CPU();
+
+        void setCode(char *filename);
+        void setCode(Vector<int> code);
+        void setRegisters(Vector<int> registers);
+        int* readRegisters();
+        void execute();
+
+    private:
+
+        Stack<int> stack_;
+        Stack<int> return_;
+        Vector<int> code_;
+        Vector<int> registers_;
+        Vector<int> marks_;
+
+        void mark();
+        void assemble(char *filename);
+
+    };
+}
+
+#endif // CPU_H_INCLUDED
